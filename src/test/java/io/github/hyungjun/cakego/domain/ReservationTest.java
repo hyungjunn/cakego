af8373b83bb71a6c314a0a487e90ebed3d7b3e44
hyungjunn <hotfirebulldog@gmail.com>
2025-05-15 12:09:10 +0900

    docs: write reservation test comment

---
+++ src/test/java/io/github/hyungjun/cakego/domain/ReservationTest.java
@@ -0,0 +1,20 @@
+package io.github.hyungjun.cakego.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class ReservationTest {
+    @Test
+    void shouldStartAsPendingWhenReservationIsCreated() {
+        // 1. 케이크 사이즈 객체 생성
+
+        // 2. 고객 객체 생성
+
+        // 3. 옵션 선택
+
+        // 4. 픽업 날짜 객체 생성
+
+        // 5. 예약 객체 생성
+
+        // 6. 예약 상태 확인
+    }
+}
