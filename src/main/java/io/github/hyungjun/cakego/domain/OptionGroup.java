b8ec72612c8d50b244b0010f85f25a877d669828
hyungjunn <hotfirebulldog@gmail.com>
2025-05-23 22:46:58 +0900

    remove equals and hashcode because of not value object

--- src/main/java/io/github/hyungjun/cakego/domain/OptionGroup.java
+++ src/main/java/io/github/hyungjun/cakego/domain/OptionGroup.java
@@ -3,7 +3,6 @@ package io.github.hyungjun.cakego.domain;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Getter
 public class OptionGroup {
@@ -11,15 +10,4 @@ public class OptionGroup {
     private String name;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof OptionGroup that)) return false;
-        return Objects.equals(name, that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(name);
-    }
 }
