061d383dd3c858d705ad577f3b6ce5ff78c5cedf
hyungjunn <hotfirebulldog@gmail.com>
2025-06-18 16:19:32 +0900

    add ShopHoliday Entity

---
+++ src/main/java/io/github/hyungjun/cakego/domain/ShopHoliday.java
@@ -0,0 +1,9 @@
+package io.github.hyungjun.cakego.domain;
+
+import java.time.LocalDate;
+
+public class ShopHoliday {
+    private Long id;
+    private Shop shop;
+    private LocalDate holidayDate;
+}
