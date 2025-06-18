09a1f30ee2a71d4af28728cfd3c13cd92e44114d
hyungjunn <hotfirebulldog@gmail.com>
2025-06-18 16:13:18 +0900

    add isActive comment in Shop Entity
    논리적 삭제(Soft delete)를 위한 상태 플래그를 어떻게 처리해야 할지 주석으로 표시

--- src/main/java/io/github/hyungjun/cakego/domain/Shop.java
+++ src/main/java/io/github/hyungjun/cakego/domain/Shop.java
@@ -12,7 +12,7 @@ public class Shop {
     private String businessRegistrationNumber;
     private String shopImageUrl;
     private String openingHours;
-    private boolean isActive; // 기본값 true
+    private boolean isActive; // 기본값 true, nullable = false, shop을 삭제할 때 isActive만 false로 한 다음 저장시키기, @where
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
